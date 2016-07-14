5b0ac1e958fbe23cf2c0be9e5fabdcad1c956c21
Thamizharasu <zarub2k@gmail.com>
2016-07-14 23:06:17 +0530

    Code clean-up is done

--- wear/src/main/java/com/cloudskol/weardroid/MainActivity.java
+++ wear/src/main/java/com/cloudskol/weardroid/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
